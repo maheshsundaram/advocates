24986322d81d237c85a435851c89836a61d2274c
Mahesh Sundaram <mahesh@omkara.dev>
2025-01-26 16:58:30 -1000

    Add types and constants for fields

--- src/db/seed/advocates.ts
+++ src/db/seed/advocates.ts
@@ -1,34 +1,6 @@
 import db from "..";
 import { advocates } from "../schema";
-
-const specialties = [
-  "Bipolar",
-  "LGBTQ",
-  "Medication/Prescribing",
-  "Suicide History/Attempts",
-  "General Mental Health (anxiety, depression, stress, grief, life transitions)",
-  "Men's issues",
-  "Relationship Issues (family, friends, couple, etc)",
-  "Trauma & PTSD",
-  "Personality disorders",
-  "Personal growth",
-  "Substance use/abuse",
-  "Pediatrics",
-  "Women's issues (post-partum, infertility, family planning)",
-  "Chronic pain",
-  "Weight loss & nutrition",
-  "Eating disorders",
-  "Diabetic Diet and nutrition",
-  "Coaching (leadership, career, academic and wellness)",
-  "Life coaching",
-  "Obsessive-compulsive disorders",
-  "Neuropsychological evaluations & testing (ADHD testing)",
-  "Attention and Hyperactivity (ADHD)",
-  "Sleep issues",
-  "Schizophrenia and psychotic disorders",
-  "Learning disorders",
-  "Domestic abuse",
-];
+import { specialties } from "@/types";
 
 const randomSpecialty = () => {
   const random1 = Math.floor(Math.random() * 24);
