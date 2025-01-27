15eb0aa31b5ad9f674a1e3d54a35cc20ffc73ef5
Mahesh Sundaram <mahesh@omkara.dev>
2025-01-26 16:36:41 -1000

    Fix database setup

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,19 +1,18 @@
 import { drizzle } from "drizzle-orm/postgres-js";
+import { PostgresJsDatabase } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
+import * as schema from "./schema";
+
+let db: PostgresJsDatabase<typeof schema> | null = null;
 
 const setup = () => {
-  if (!process.env.DATABASE_URL) {
-    console.error("DATABASE_URL is not set");
-    return {
-      select: () => ({
-        from: () => [],
-      }),
-    };
-  }
+  if (db) return db;
 
-  // for query purposes
+  if (!process.env.DATABASE_URL) throw new Error("DATABASE_URL is not set");
+
   const queryClient = postgres(process.env.DATABASE_URL);
-  const db = drizzle(queryClient);
+  db = drizzle(queryClient, { schema });
+
   return db;
 };
 
